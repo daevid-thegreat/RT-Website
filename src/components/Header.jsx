8734ef69a8fc7d5f8b9b13b3f3a2411044860370
Howard Otuya <howardotuya17@gmail.com>
2024-04-11 12:10:42 +0100

    switched from react-head to react-helmet

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,29 +1,27 @@
-import { HeadProvider, Link, Meta, Title } from "react-head";
+import { Helmet } from "react-helmet";
 
 export default function HeaderSEO({ title, url, description, image }) {
   const LinkOfImage = "https://roadtripcommunity.com" + image;
   const LinkOfUrl = "https://roadtripcommunity.com" + url;
 
   return (
-    <HeadProvider>
-      <div className="Home">
-        <Title>{title}</Title>
-        <Meta property="og:title" content={title} />
-        <Link rel="canonical" href={LinkOfUrl} />
-        <Meta name="description" content={description} />
-        <Meta property="og:description" content={description} />
-        <Meta name="keywords" content="Roadtrip, Roadtrip community" />
-        <Meta name="robots" content="index, follow" />
-        <Meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <Meta property="og:url" content={LinkOfUrl} />
-        <Meta property="og:image" content={LinkOfImage} />
-        <Meta property="og:type" content="website" />
+    <Helmet>
+      <title>{title}</title>
+      <meta property="og:title" content={title} />
+      <link rel="canonical" href={LinkOfUrl} />
+      <meta name="description" content={description} />
+      <meta property="og:description" content={description} />
+      <meta name="keywords" content="Roadtrip, Roadtrip community" />
+      <meta name="robots" content="index, follow" />
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      <meta property="og:url" content={LinkOfUrl} />
+      <meta property="og:image" content={LinkOfImage} />
+      <meta property="og:type" content="website" />
 
-        <Meta name="twitter:card" content="summary_large_image" />
-        <Meta name="twitter:title" content={title} />
-        <Meta name="twitter:description" content={description} />
-        <Meta name="twitter:image" content={LinkOfImage} />
-      </div>
-    </HeadProvider>
+      <meta name="twitter:card" content="summary_large_image" />
+      <meta name="twitter:title" content={title} />
+      <meta name="twitter:description" content={description} />
+      <meta name="twitter:image" content={LinkOfImage} />
+    </Helmet>
   );
 }
