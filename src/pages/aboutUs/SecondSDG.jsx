56f61d9371d3edc9ca466250fc0c36c263805bcd
Joshua Ogbonna <devjaykes@gmail.com>
2024-08-18 18:20:31 +0100

    chore: about page redesign implementation

--- src/pages/aboutUs/SecondSDG.jsx
+++ src/pages/aboutUs/SecondSDG.jsx
@@ -3,65 +3,72 @@ import { Box, Flex, Text, Image } from "@chakra-ui/react";
 const SecondSDG = () => {
   return (
     <Flex
-      mt={5}
-      p={{ base: 3, lg:5 }}
       flexDir="column"
       bg={"pink300"}
+      w="100%"
+      maxWidth={"1200px"}
+      mx={"auto"}
+      mt={"1rem"}
+      pl={['20px', '40px']}
+      pr={'40px'}
     >
       <Flex
         flexDir={{ base: "column", lg: "row" }}
+        w="100%"
+        py="20px"
+        justifyContent={"space-between"}
+        
       >
         <Box>
           <Text
             fontFamily="clash"
             fontSize={{ base: "24px", md: "32px" }}
             fontWeight={700}
-            // pb={{ base: "17px", md: "40px" }}
             pt={{ base: "48px", md: "20px" }}
-            pl={{ base: "none", md: "20px" }}
           >
             Our Commitment - SDG 8
           </Text>
 
           <Text
             fontFamily={"clash"}
-            fontSize={{ base: "12px", md: "16px", lg: "20px" }}
+            fontSize={'16px'}
             fontWeight={300}
             pb={"17px"}
-            pl={"20px"}
-            color={"red"}
+            color={"#752A12"}
+            letterSpacing={'-2%'}
           >
             DECENT WORK AND ECONOMIC GROWTH
           </Text>
-          
+
           <Text
             fontFamily="manrope"
-            fontSize={{ base: "14px", lg: "20px" }}
-            fontWeight={600}
+            fontSize={"16px"}
+            fontWeight={400}
             pb={{ base: "16px", md: "40px" }}
-            w={"100%"}
-            pl={{ base: "none", md: "20px" }}
+            w={['100%', '100%', '100%', '100%', "508px"]}
             pr={{ base: "none", md: "20px" }}
-
-      >
-        RoadTrip Community's vision of seeing at least 50% of our members earning $10,000 per month aligns with the United Nations Sustainable Development Goal 8 (Decent Work and Economic Growth). Through our support programs, we empower individuals to maximize job opportunities, become financially savvy, and scale their earnings, contributing to creating a more prosperous and equitable society.
-      </Text>
-
+            textAlign={"justify"}
+          >
+            RoadTrip {"Community's"} vision of seeing at least 50% of our
+            members earning $10,000 per month aligns with the United Nations
+            Sustainable Development Goal 8 (Decent Work and Economic Growth).
+            Through our support programs, we empower individuals to maximize job
+            opportunities, become financially savvy, and scale their earnings,
+            contributing to creating a more prosperous and equitable society.
+          </Text>
         </Box>
-        
+
         <Image
-            src="/images/SDG8.png"
-            alt="Green double couch with wooden legs"
-            borderWidth="3px"
-            borderStyle="solid"
-            borderColor="black"
-            objectFit="cover"
-            maxW={"908px"}
-            w={"100%"}
-            m={"auto"}
-            mt={1}
-          />
-      </Flex>    
+          src="/images/SDG8.png"
+          alt="Green double couch with wooden legs"
+          borderWidth="1px"
+          borderStyle="solid"
+          borderColor="black"
+          mt={1}
+          w={['100%', '100%', '508px']}
+          h="272px"
+        />
+      </Flex>
     </Flex>
   );
 };
--- src/pages/aboutUs/index.jsx
+++ src/pages/aboutUs/index.jsx
@@ -29,7 +29,7 @@ const AboutUs = () => {
       <FirstSDG />
       <SecondSDG />
       <Team />
-      <Mentors />
+      {/* <Mentors /> */}
       <Volunteers />
     </>
   );
